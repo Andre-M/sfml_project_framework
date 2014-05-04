db04cd76e1300a9042ea97a68799060a402573f9
Andre C Meireles <andre.meireles@knights.ucf.edu>
2014-05-03 22:26:53 -0400

    introduce vs 2013 solution

---
+++ framework_solution/framework_solution/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Main!!! " << std::endl;
+
+	return 0;
+}
+
+int returns_nine()
+{
+	return 9;
+}
+
+bool returns_false()
+{
+	return false;
+}
+
+
